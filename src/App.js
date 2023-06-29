056a59f3b3fa0135568cce68670eb28d0d89d35f
nansiiii <nasrin.shakery8@gmail.com>
2023-06-29 12:42:09 +0330

    nav component and Link

--- src/App.js
+++ src/App.js
@@ -11,28 +11,22 @@ import style from './styles/style.module.css';
 
 
 class App extends Component {
-  state = {  } 
+  constructor(props) {
+    super(props);
+    this.state ={
+      
+    }
+  }
   render() { 
     return (
       <>
-        <div className={style["navbar"]}>
-          <ul className={style["nav"]}>
-            <li className={style['nav-btn']}><a href="/" className={style['a-link']}>Home</a></li>
-            <li className={style['nav-btn']}><a href="movies" className={style['a-link']}>Movies</a></li>
-            <li className={style['nav-btn']}><a href="aboutus" className={style['a-link']}>About Us</a></li>
-          </ul>
-        </div>
-        <hr className={style.hr} />
-        <div className={style.slider}>
+        <Navbar></Navbar>
 
-        </div>
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
-          <Route path='/Movies' element={<Movies></Movies>}></Route>
-          <Route path='/AboutUs' element={<AboutUs></AboutUs>}></Route>
+          <Route path='movies' element={<Movies></Movies>}></Route>
+          <Route path='aboutus' element={<AboutUs></AboutUs>}></Route>
         </Routes>
-        
-        
         
       </>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import style from '../styles/style.module.css';
 
 class Navbar extends Component {
@@ -9,9 +11,9 @@ class Navbar extends Component {
             <>
                 <div className={style["navbar"]}>
                   <ul className={style["nav"]}>
-                    <li className={style['nav-btn']}><a href="#" className={style['a-link']}>Home</a></li>
-                    <li className={style['nav-btn']}><a href="#" className={style['a-link']}>Movies</a></li>
-                    <li className={style['nav-btn']}><a href="#" className={style['a-link']}>About Us</a></li>
+                    <li className={style['nav-btn']}><Link to="/" className={style['a-link']}>Home</Link></li>
+                    <li className={style['nav-btn']}><Link to="movies" className={style['a-link']}>Movies</Link></li>
+                    <li className={style['nav-btn']}><Link to="aboutus" className={style['a-link']}>About Us</Link></li>
                   </ul>
                 </div>
                 <hr className={style.hr} />
